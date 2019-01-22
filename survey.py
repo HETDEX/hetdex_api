ffbfd399b24a562ebe64f1de86c9915b4eca9d74
grzeimann <grzeimann@gmail.com>
2019-01-22 11:17:33 -0600

    first commit

---
+++ survey.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 11:02:53 2019
+
+@author: gregz
+"""
+
+import tables as tb
+import sys
+
+
+class Survey:
+    def __init__(self, survey):
+        '''
+        Initialize the Survey class for a given data release
+
+        Input
+        -----
+        survey : string
+            Data release you would like to load, i.e., 'DR1' or 'Parallel'.
+            This is case insensitive.
+        '''
+        survey_options = {'dr1': '/work/05350/ecooper/maverick/gettar/survey_test.h5',
+                          'parallel': 'PATHNAME'}
+        if survey.lower not in survey_options:
+            print('survey not in survey options')
+            sys.exit(1)
+        self.filename = survey_options[survey]
+        self.hdfile = tb.open_file(self.filename, mode='r')
+        colnames = self.hdfile.root.Survey.colnames
+        for name in colnames:
+            setattr(self, name, getattr(self.hdfile.root.Survey.cols, name)[:])
