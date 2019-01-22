d7434c8a18d3b1820642782a58b26b0a1ac11624
grzeimann <grzeimann@gmail.com>
2019-01-22 09:53:31 -0600

    new name

--- hdf5_visualize_ds9.py
+++ hdf5_visualize_ds9.py
@@ -49,7 +49,9 @@ def main(argv=None):
                            % args.hdf5_file)
             return None
     if args.show:
-        print(h5file.root.Info)
+        print(h5file.root.Info.Fibers)
+        print(h5file.root.Info.Shot)
+        print(h5file.root.Info.Images)
         return None
 
     if args.extension is None:
