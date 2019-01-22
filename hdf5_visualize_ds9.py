b206ad216740d4a1011015677f50092279451329
grzeimann <grzeimann@gmail.com>
2019-01-22 09:57:33 -0600

    new name

--- hdf5_visualize_ds9.py
+++ hdf5_visualize_ds9.py
@@ -50,6 +50,8 @@ def main(argv=None):
             return None
     if args.show:
         print(h5file.root.Info.Fibers.cols)
+        for array in h5file.walk_nodes("/Info.Fibers", "Array"):
+            print(array)
         print(h5file.root.Info.Shot.cols)
         print(h5file.root.Info.Images.cols)
         return None
