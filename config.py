efeb3d6591d688d7d412b3cf473f69d9147260fe
grzeimann <grzeimann@gmail.com>
2019-03-08 12:39:43 -0600

    hard links of folder structure for other codes to use

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ Created on Fri Mar  8 12:21:35 2019
 """
 import os.path as op
 
-hdr_dir = 'work/03946/hetdex/hdr1'
+hdr_dir = '/work/03946/hetdex/hdr1'
 software_dir = op.join(hdr_dir, 'software')
 red_dir = op.join(hdr_dir, 'reduction')
 data_dir = op.join(red_dir, 'data')
