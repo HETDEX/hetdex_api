e5659ad5d23e7ff4274d11285d5e597c0f520552
eriniocentric <erin.jm.cooper@gmail.com>
2019-03-15 11:14:48 -0500

    added paths needed for create_survey

--- config.py
+++ config.py
@@ -11,3 +11,13 @@ hdr_dir = '/work/03946/hetdex/hdr1'
 software_dir = op.join(hdr_dir, 'software')
 red_dir = op.join(hdr_dir, 'reduction')
 data_dir = op.join(red_dir, 'data')
+tp_dir = op.join(red_dir, 'throughput')
+calib_dir = op.join(hdr_dir, 'calib')
+astrometry_dir = "/work/00115/gebhardt/maverick/vdrp/shifts/"
+
+path_gpinfo = '/work/03030/grnagara/maverick/getgp2/MasterFWHM.txt'
+path_acc_flags = op.join(red_dir, 'status_summary_hdr1.txt')
+path_radec = op.join(calib_dir, 'radec.all')
+
+survey_list = op.join(red_dir, 'hdr1.scilist')
+cal_list = op.join(red_dir, 'hdr1.callist')
