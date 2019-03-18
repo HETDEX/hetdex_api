ab1a51bcf4c8f3a58925e0b747a3184490d745ba
Daniel Farrow <dfarrow@login1.wrangler.tacc.utexas.edu>
2019-03-18 13:06:15 -0500

    Changed to look for VDRP filename style

--- hdr1_bash_scripts/generate_sensitivity_cube_hdf5s.bash
+++ hdr1_bash_scripts/generate_sensitivity_cube_hdf5s.bash
@@ -8,7 +8,7 @@
 #
 
 # Script to compute averages
-AV_FLIM_SCRIPT=/home/04120/dfarrow/HETDEX_API/compute_average_flux_limit.py 
+AV_FLIM_SCRIPT=/work/04120/dfarrow/wrangler/flims/hdr1_code/HETDEX_API/compute_average_flux_limit.py 
 
 INPATH=/work/04120/dfarrow/wrangler/flims/hdr1_fits_cubes
 OUTPATH=/work/04120/dfarrow/wrangler/flims/hdr1
@@ -16,13 +16,13 @@ OUTPATH=/work/04120/dfarrow/wrangler/flims/hdr1
 # File in to which to write the biweight location of the flux limits at 4540AA for each shot
 AVERAGE_FLIM_FILE=/work/04120/dfarrow/wrangler/flims/hdr1/average_flims.txt
 
-for fpath in `ls -d ${INPATH}/20*v*`
+for fpath in `ls -d ${INPATH}/2019*v*`
 do
     datevobs=`basename ${fpath}`
     echo $datevobs
     pushd $fpath
     cd flim
-    python $AV_FLIM_SCRIPT --fout "${outfolder}_ifu_flims.txt" --fn-shot-average $AVERAGE_FLIM_FILE *.fits
-    add_sensitivity_cube_to_hdf5 *_3d.fits "${OUTPATH}/${datevobs}_sensitivity_cube.h5"
+    python $AV_FLIM_SCRIPT --fn-shot-average $AVERAGE_FLIM_FILE *.fits
+    add_sensitivity_cube_to_hdf5 *.fits "${OUTPATH}/${datevobs}_sensitivity_cube.h5"
     popd
 done
