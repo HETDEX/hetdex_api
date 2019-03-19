8ad2402b6503b9bd3bf6d3678084096a8d704407
eriniocentric <erin.jm.cooper@gmail.com>
2019-03-18 20:23:19 -0500

    Added many many more files into the creation script, added some metadata, logging info

--- create_astrometry_hdf5.py
+++ create_astrometry_hdf5.py
@@ -36,6 +36,16 @@ class QualityAssessment(tb.IsDescription):
     yrms = tb.Float32Col(pos=4)
     nstars = tb.Float32Col(pos=5)
 
+class NominalVals(tb.IsDescription):
+    expnum = tb.Int32Col(pos=0)
+    ra = tb.Float32Col(pos=1)
+    dec = tb.Float32Col(pos=2)
+    parangle = tb.Float32Col(pos=3)
+    x_dither_pos = tb.Float32Col(pos=4)
+    y_dither_pos = tb.Float32Col(pos=5)
+    norm = tb.Float32Col(pos=6)
+    als_filename = tb.StringCol((23))
+
 
 def main(argv=None):
     ''' Main Function '''
@@ -81,40 +91,106 @@ def main(argv=None):
     groupDithall = fileh.create_group(groupAstrometry, 'Dithall', 'Fiber Astrometry Info')
     groupOffsets = fileh.create_group(groupAstrometry, 'PositionOffsets', 
                                       'Offset in star matches')
+    groupMathes = fileh.create_group(groupAstrometry, 'CatalogMatches', 'Match Catalog Info')
 
     tableQA = fileh.create_table(groupAstrometry, 'QA', QualityAssessment, 
                              'Qulity Assessment')
+    tableNV = fileh.create_table(groupAstrometry,'NominalVals', NominalVals,
+                                 'Nominal Values')
+
+    # store shuffle.cfg and DATEvOBS.log files as simple text arrays
+
+    fileshuffle = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),'shuffle.cfg')
+    try:
+        f = open(fileshuffle, 'r')
+        shuffle = fileh.create_array(groupAstrometry, 'ShuffleCfg', f.read())
+        shuffle.set_attr('filename', fileshuffle)
+        f.close()
+    except:
+        args.log.warning('Could not include %s' % fileshuffle)
+
+    logfile = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),
+                      str(args.date) + 'v' + str(args.observation).zfill(3) + '.log')
+    try: 
+        f = open(logfile)
+        fileh.create_array(groupAstrometry, 'LogInfo', f.read())
+        f.close()
+    except: 
+        args.log.warning('Could not include %s' % logfile)
+
+        
+    # store fplane table
+
     filefplane = op.join(args.rootdir, str(args.date) + "v" + str(args.observation).zfill(3),
-                         'fplane.txt')
-    
+                         'fplane.txt')    
     try:
         f = ascii.read(filefplane, names=['ifuslot', 'fpx', 'fpy', 'specid',
                                           'specslot', 'ifuid', 'ifurot', 'platesc'])
         fplanetable = fileh.create_table(groupAstrometry, 'fplane', f.as_array())
+        fplanetable.set_attr('filename', filefplane)
     except:
         args.log.warning('Could not include %s' % filefplane)
 
+    # store catalogs of stars used in astrometric fit
+
     file_stars = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3), 
                          'shout.ifustars')    
-    
     try:
         f_stars = ascii.read(file_stars, names=['ignore', 'star_ID', 'ra_cat', 'dec_cat',
                                                 'u', 'g', 'r', 'i', 'z'])
-        starstable = fileh.create_table(groupAstrometry, 'StarCatalog', f_stars.as_array()) 
+        starstable = fileh.create_table(groupAstrometry, 'StarCatalog', f_stars.as_array())
+        starstable.set_attr('filename',file_stars)
+        if any(f_stars['z'] > 0):
+            starstable.set_attr('source','SDSS')
+        else:
+            starstable.set_attr('source','GAIA')
     except:
         args.log.warning('Could not include %s' % file_stars)
-    
     
     pngfiles = glob.glob(op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3), '*.png'))
     pngnum = 1
 
     for pngfile in pngfiles:
-        plt_image = plt.imread(pngfile)                         
-        pngim = fileh.create_array(groupCoadd, 'png_exp' + str(pngnum).zfill(2), plt_image) 
+        plt_image = plt.imread(pngfile)
+        pngim = fileh.create_array(groupCoadd, 'png_exp' + str(pngnum).zfill(2), plt_image)
         pngim.attrs['CLASS'] = 'IMAGE'
+        pngim.attrs['filename'] = pngfile
         pngnum += 1
 
-    for expn in ['exp01', 'exp02', 'exp03']:
+    fileallmch = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),'all.mch')
+    try:
+        allmch = ascii.read(fileallmch)
+    except:
+        args.log.warning('Could not include %s' % fileallmch)
+
+        
+    filenorm = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),'norm.dat')
+    try:
+        norm = ascii.read(filenorm)
+    except:
+        args.log.warning('Could not include %s' % filenorm)
+
+    # index over dithers to gather diher specific info    
+    for idx, expn in enumerate(['exp01', 'exp02', 'exp03']):
+
+        radecfile = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3), 
+                             'radec2_' + expn + 'dat')
+        try:
+            rowNV = tableNV.row
+            radec = ascii.read(radecfile)
+            rowNV['expnum'] = int(expn[3:5])
+            rowNV['ra'] = radec['col1']
+            rowNV['dec'] = radec['col2']
+            rowNV['parangle'] = radec['col3']
+            rowNV['norm'] = filenorm[idx]
+            rowNV['x_dither_pos'] = allmch['col3'][idx]
+            rowNV['y_dither_pos'] = allmch['col4'][idx]
+            rowNV['als_filename'] = allmch['col1'][idx]
+            rowNV.append()
+        except:
+            args.log.warning('Could not include NV table info for dither '+ expn)
+
+
         fitsfile = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),
                            str(args.date) + 'v' + str(args.observation).zfill(3)
                            + 'fp_' + expn + '.fits')
@@ -126,48 +202,66 @@ def main(argv=None):
             fitsim.attrs['CLASS'] = 'IMAGE'
             fitsim.attrs['IMAGE_MINMAXRANGE'] = (-1.5, 100)
             fitsim.attrs['HEADER'] = F[0].header
+            fitsim.attrs['filename'] = fitsfile
             F.close()
 
 
-            # populate offset info for catalog matches
-            file_getoff = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),
-                          'getoff_' + expn + '.out')
+        # populate offset info for catalog matches
+        file_getoff = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),
+                              'getoff_' + expn + '.out')
             
-            try:
-                f_getoff = ascii.read(file_getoff, names=['xoffset', 'yoffset', 'ra_dex',
-                                                          'dec_dex', 'ra_cat', 'dec_cat',
-                                                          'ifuslot'])
-                getoffinfo = fileh.create_table(groupOffsets, expn, f_getoff.as_array())
-            except:
-                args.log.warning('Could not include %s' % file_getoff)
+        try:
+            f_getoff = ascii.read(file_getoff, names=['xoffset', 'yoffset', 'ra_dex',
+                                                      'dec_dex', 'ra_cat', 'dec_cat',
+                                                      'ifuslot'])
+            getoffinfo = fileh.create_table(groupOffsets, expn, f_getoff.as_array())
+            getoffinfo.set_attr('filename', file_getoff)
+        except:
+            args.log.warning('Could not include %s' % file_getoff)
             
 
-            # populate fiber astrometry data
-            file_dith = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),
-                                'dith_' + expn + '.all')    
-            try:
-                f_dith = ascii.read(file_dith)
-                dithinfo = fileh.create_table(groupDithall, expn, f_dith.as_array())
-            except:
-                args.log.warning('Could not include %s' % file_dith)
+        # populate fiber astrometry data
+        file_dith = op.join(args.rootdir, str(args.date) + 'v' + str(args.observation).zfill(3),
+                            'dith_' + expn + '.all')    
+        try:
+            f_dith = ascii.read(file_dith)
+            dithinfo = fileh.create_table(groupDithall, expn, f_dith.as_array())
+            dithinfo.set_attr('filename', file_dith)
+        except:
+            args.log.warning('Could not include %s' % file_dith)
+            
+        # populate median and rms in offsets for quality assessment purposes
 
-            # populate median and rms in offsets for quality assessment purposes
-            file_getoff2 = op.join(args.rootdir, str(args.date) + 'v'
-                                   + str(args.observation).zfill(3), 'getoff2_' + expn + '.out')
-            try:
-                f_getoff2 = ascii.read(file_getoff2)
-                row = tableQA.row
-                row['expnum'] = int(expn[3:5])
-                row['xoffset'] = f_getoff2['col1']
-                row['yoffset'] = f_getoff2['col2']
-                row['xrms'] = f_getoff2['col3']
-                row['yrms'] = f_getoff2['col4']
-                row['nstars'] = f_getoff2['col5']
-                row.append()
-            except:
-                args.log.warning('Could not include %s' % file_getoff2)
-            tableQA.flush()
+        file_getoff2 = op.join(args.rootdir, str(args.date) + 'v'
+                               + str(args.observation).zfill(3), 'getoff2_' + expn + '.out')
+        try:
+            f_getoff2 = ascii.read(file_getoff2)
+            row = tableQA.row
+            row['expnum'] = int(expn[3:5])
+            row['xoffset'] = f_getoff2['col1']
+            row['yoffset'] = f_getoff2['col2']
+            row['xrms'] = f_getoff2['col3']
+            row['yrms'] = f_getoff2['col4']
+            row['nstars'] = f_getoff2['col5']
+            row.append()
+        except:
+            args.log.warning('Could not include %s' % file_getoff2)
+        
+        
+        file_xy = op.join(args.rootdir, str(args.date) + 'v'
+                          + str(args.observation).zfill(3), 'xy_' + expn + '.dat')
+        try:
+            xy_table = ascii.read(file_xy)
+            fileh.create_table(groupMatches, expn, xy.as_array())
+        except:
+            args.log.warning('Could not include %s' % file_xy)
 
+    tableQA.set_attr('filename', 'getoff2_exp??.out')
+    tableNV.set_attr('dither_file', 'all.mch')
+    tableNV.set_attr('norm_file', 'norm.dat')
+    tableNV.set_attr('radec_file', 'radec2_exp??.dat')
+    tableQA.flush()
+    tableNV.flush()
     fileh.close()
 
 
