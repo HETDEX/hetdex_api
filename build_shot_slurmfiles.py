70cc749f3cbf41aef2866b6ef2019ee391845a0f
grzeimann <grzeimann@gmail.com>
2019-03-19 16:44:30 -0500

    adjusting for all shotfiles

--- build_shot_slurmfiles.py
+++ build_shot_slurmfiles.py
@@ -42,5 +42,5 @@ for i, object_chunk in enumerate(object_chunks):
     subprocess.call([sedcall], shell=True)
     d1 = D + datetime.timedelta(0, 0, 600*(i+1))
     d2 = d1.strftime('%H:%M %B %d')
-    atcall.append('echo "source ~hetdex/.bashrc; sbatch %s" | at %s' % (sname, d2))
+    atcalls.append('echo "source ~hetdex/.bashrc; sbatch %s" | at %s' % (sname, d2))
 G.write('\n'.join(atcalls))
