5802f7afa87f634ea1b559c7df7e2746caea1e28
eriniocentric <erin.jm.cooper@gmail.com>
2019-03-22 12:59:05 -0500

    Adapted mkdh5 script for stampede2

---
+++ hdr1_bash_scripts/mkhdf5_stampede2.sh
@@ -0,0 +1,6 @@
+# bash script to run the HDF5 shot creation scripts on stampede2 
+cd /scratch/03946/hetdex/
+python /work/03946/hetdex/hdr1/software/HETDEX_API/create_shot_hdf5.py -d $1 -o $2 -of $1v$2.h5
+python /work/03946/hetdex/hdr1/software/HETDEX_API/create_astrometry_hdf5.py -d $1 -o $2 -of $1v$2.h5 --append
+python /work/03946/hetdex/hdr1/software/HETDEX_API/create_cal_hdf5.py -d $1 -o $2 -of $1v$2.h5 --append
+mv $1v$2.h5 /work/03946/hetdex/hdr1/reductions/data/
