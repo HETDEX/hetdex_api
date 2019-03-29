fb6ac350b2a86c04f422f575d808f64cd68c7e7f
eriniocentric <erin.jm.cooper@gmail.com>
2019-03-29 13:38:40 -0500

    added config.detecth5

--- hetdex_api/detections.py
+++ hetdex_api/detections.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 """
+
+Initiates the Detections class. 
+
+An API may or may not be developed in the future.
+We recommend just using pytables since this is only one file.
+
 Created on 2019/01/28
 
 @author: Erin Mentuch Cooper
 """
 
 import tables as tb
+
+import config
 
 
 class Detections:
@@ -19,7 +27,7 @@ class Detections:
             Data release you would like to load, i.e., 'DR1' or 'Parallel'.
             This is case insensitive.
         '''
-        survey_options = {'dr1': '/work/05350/ecooper/wrangler/database/detect_test.h5',
+        survey_options = {'dr1': config.detecth5,
                           'parallel': 'PATHNAME'}
         if survey.lower() not in survey_options:
             print('survey not in survey options')
