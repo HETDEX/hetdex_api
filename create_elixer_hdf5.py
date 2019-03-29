3aab6237172f507b15783a6b662c8f00b6a03c18
eriniocentric <erin.jm.cooper@gmail.com>
2019-03-29 18:02:47 -0500

    Added imaging ingestion

--- create_elixer_hdf5.py
+++ create_elixer_hdf5.py
@@ -2,7 +2,7 @@
 """
 Created: 2019/03/28
 
-@author: Erin Mentuch Cooper
+@authors: Erin Mentuch Cooper, Dustin Davis
 
 This file contains limited information related to the ELIXER output
 for HETDEX  line detections
@@ -27,27 +27,90 @@ import subprocess
 import numpy as np
 import tables as tb
 
+import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
 from astropy.io import ascii
 from astropy.io import fits
 import astropy.units as u
 from input_utils import setup_logging
 
-def read_elixer_cat(catfn):
+from hetdex_api import config
 
+class Detection:
+    #represents a single detection (may or may not be an acceptable LAE
+    def __init__(self):
+
+        self.detectid = None
+        self.entryid = None #unique for a run?
+        self.pdfname = None
+        self.detectname = None #i.e. 20180123v009_137
+        self.ra = None
+        self.dec = None
+        self.w = None #observed wavelength
+
+        self.z = None #assumes LyA
+
+        self.ew_obs = None
+        self.ew_rest = None #assuming LyA z
+        self.line_flux = None
+
+        self.sigma = None
+        self.chi2 = None
+        self.continuum = None
+
+        self.plae_poii_hetdex = None #hetdex only data
+        self.plae_poii_aperture = None
+        self.aperture_mag = None
+        self.aperture_filter = None
+
+        self.neighbors = [] #empty list (of Neighbors
+
+    @property
+    def num_neighbors(self):
+        try:
+            return len(self.neighbors)
+        except:
+            return -1
+
+
+class Neighbor:
+    #a catalog neighbor
+    def __init__(self):
+        self.ra = None
+        self.dec = None
+        self.mag = None
+        self.filter = None #filter for the magnitude
+        self.dist = None #distance in arcsecs
+        self.plae_poii_cat = None #P(LAE)/P(OII)
+
+
+def read_elixer_catalogs(fibfn, catfn):
     """
-    read  _cat.txt catalogs
+    read the _fib.txt and _cat.txt catalogs
     create Detections for each (combining across and within catalogs as needed)
 
     :return: list of Detections
-    """
-    
-    detections = []
 
-    with open(catfn,"r") as f:
+    Author: Dustin Davis
+    """
+
+    detections = []
+    fib_version = None #can switch on version number if necessary
+    cat_version = None
+
+    #should be one line per detection in fib.txt
+    #at least one line but may be more in cat.txt
+
+    #read fib.txt first and build out list of detections
+    with open(fibfn,"r") as f:
         for line in f:
-            if line[0] == '#':
+            if line[0] == '#': #except get # version 1.5.0a16
+                if (fib_version is None) and ('version' in line): #1st line should be the version
+                    toks = line.split()
+                    if (toks is not None) and (len(toks)== 3):
+                        fib_version = toks[2]
                 continue
+
             if len(line) < 100:
                 continue
 
@@ -56,50 +119,90 @@ def read_elixer_cat(catfn):
             if len(toks) < 29: #must be at least 29 for _fib.txt
                 continue
 
-            pdfname = toks[0]
-            detectid = np.int64(toks[1])
-            entryid = toks[2]
+            d = Detection()
 
-            ra = float(toks[3])
-            dec = float(toks[4])
+            d.pdfname = toks[0]
+            d.detectid = np.int64(toks[1])
+            d.entryid = toks[2]
+            d.ra = float(toks[3])
+            d.dec = float(toks[4])
+            d.w = float(toks[5])
+            d.sigma = float(toks[6])
+            d.chi2 = float(toks[7])
+            d.line_flux = float(toks[10])
+            d.continuum = float(toks[12])
 
-            num_cat_matches = int(toks[5])
+            d.plae_poii_hetdex = float(toks[14])
 
-            w = float(toks[6])
+            detections.append(d)
 
-            match_ra = float(toks[24])
-            match_dec = float(toks[25])
-            dist = float(toks[26])
-            mag = toks[28]
-            filter = toks[29]
-            if (toks[32] is not None) and (toks[32].lower() != 'none'): #can be None if could not get an aperture
-                plae_poii = float(toks[32])
-            else:
-                plae_poii = -1 #this does me no good here, so skip this line
-                print("Bad (None) PLAE/POIU. Skipping %s %s %s" %(pdfname,str(detectid),entryid))
+
+    #then read cat.txt and match up to existing entry in detections list
+    with open(catfn,"r") as f:
+        for line in f:
+            if line[0] == '#':
+                if (cat_version is None) and ('version' in line): #1st line should be the version
+                    toks = line.split()
+                    if (toks is not None) and (len(toks)== 3):
+                        cat_version = toks[2]
                 continue
 
-            #find your mates:
-            #there may be several ... the 1st is the aperture
-            #each additional one is a catalog match
-            for m in detections:
-                if (m.entryid == entryid) and (m.detectid == detectid) and (m.ra == ra) and (m.dec == dec) and (m.w == w):
-                    #match
-                    if (match_ra == 666) and (match_dec == 666) and (dist == 0.0): #this is the aperture entry
-                        m.plae_poii_aperture = plae_poii
-                        m.aperture_filter = filter
-                        m.aperture_mag = mag
-                    else: #this is a matched catalog object
-                        n = Neighbor()
-                        n.ra = match_ra
-                        n.dec = match_dec
-                        n.mag = mag
-                        n.filter = filter  # filter for the magnitude
-                        n.dist = dist  # distance in arcsecs
-                        n.plae_poii = plae_poii  # P(LAE)/P(OII)
+            if len(line) < 100:
+                continue
 
-                        m.num_neighbors += 1
-                        m.neighbors.append(n)
+            toks = line.split() #white space split
+
+            if len(toks) < 29: #must be at least 29 for _fib.txt
+                continue
+
+            
+            pdfname = toks[0]
+            detectid = np.int64(toks[1])
+                
+            try:
+                entryid = toks[2]
+                
+                ra = float(toks[3])
+                dec = float(toks[4])
+                
+                num_cat_matches = int(toks[5])
+                
+                w = float(toks[6])
+                
+                match_ra = float(toks[24])
+                match_dec = float(toks[25])
+                dist = float(toks[26])
+                mag = toks[28]
+                filter = toks[29]
+                if (toks[32] is not None) and (toks[32].lower() != 'none'): #can be None if could not get an aperture
+                    plae_poii = float(toks[32])
+                else:
+                    plae_poii = -1 
+                    
+                #find your mates:
+                #there may be several ... the 1st is the aperture
+                #each additional one is a catalog match
+
+                for m in detections:
+                    if (m.entryid == entryid) and (m.detectid == detectid) and (m.ra == ra) and (m.dec == dec) and (m.w == w):
+                        #match
+                        if (match_ra == 666) and (match_dec == 666) and (dist == 0.0): #this is the aperture entry
+                            m.plae_poii_aperture = plae_poii
+                            m.aperture_filter = filter
+                            m.aperture_mag = mag
+                        else: #this is a matched catalog object
+                            n = Neighbor()
+                            n.ra = match_ra
+                            n.dec = match_dec
+                            n.mag = mag
+                            n.filter = filter  # filter for the magnitude
+                            n.dist = dist  # distance in arcsecs
+                            n.plae_poii = plae_poii  # P(LAE)/P(OII)
+                            
+                            m.num_neighbors += 1
+                            m.neighbors.append(n)
+            except:
+                print('Could not ingest cat info for %s' % detectid) 
 
     #Now we have consumed the catalog files
     return detections
@@ -112,6 +215,18 @@ class Classifications(tb.IsDescription):
     plae_poii_cat = tb.Float32Col()
     cat_src = tb.StringCol((20))
     
+
+def get_elixer_image(detectid, elix_path):
+    file_jpg = op.join(elix_path, 'jpgs', str(detectid) + '.jpg')
+    file_pdf = op.join(elix_path, 'pdfs', str(detectid) + '.pdf')
+    if op.exists(file_jpg):
+        elixim = plt.imread(file_jpg)
+    elif op.exists(file_pdf):
+        file_png = detectid + '.png'
+        os.system('pdftoppm ' + file_pdf + ' ' + file_png + ' -png -singlefile')
+        elixim = plt.imread(file_png)
+    return elixim
+
 def main(argv=None):
     ''' Main Function '''
     # Call initial parser from init_utils
@@ -120,7 +235,7 @@ def main(argv=None):
 
     parser.add_argument("-df", "--detectfile",
                         help='''Provide HDF5 of detections''',
-                        type=str, default='/work/05350/ecooper/hdr1/detect/detect_hdr1.h5')
+                        type=str, default=config.detecth5)
     
     parser.add_argument("-dets", "--dets",
                         help='''List of detections in form DATEvSHOT_inputID''',
@@ -136,7 +251,7 @@ def main(argv=None):
     
     parser.add_argument("-ep", "--elixer_path",
                         help='''Path to elixer output''',
-                        type=str, default='/work/05350/ecooper/stampede2/elixer/')
+                        type=str, default='/scratch/03261/polonius/')
 
     parser.add_argument("-name", "--elixer_name",
                         help='''Path to elixer output''',
@@ -147,22 +262,20 @@ def main(argv=None):
     args.log = setup_logging()
 
     # open elixer _cat.txt file to be ingested                                                
-    catfn = op.join(args.elixer_path, str(args.elixer_name) + '_cat.txt')
+    catfn = op.join(args.elixer_path, 'catalogs', '2017xx_cat.txt')
+    fibfn = op.join(args.elixer_path, 'catalogs', '2017xx_fib.txt')
 
     if op.exists(catfn):
-        elixer_info = read_elixer_cat(catfn)
+        elixer_info = read_elixer_catalogs(fibfn, catfn)
     else:
         print('Could not open %s' % catfn)
 
+    filedet = tb.open_file(config.detecth5, 'r')
+    detect_list = filedet.root.Detections.cols.detectid[:]
+    filedet.close()
+
     # open elixer HDF5 file, if append option is given this will be a group added
     # to an existing exlier HDF5 file
-    try:
-        filedet = tb.open_file(args.detectfile, 'r')
-        detect_list = filedet.root.Detections.cols.detectid[:]
-        filedet.close()
-    except:
-        args.log.warning('Could not open %s' % args.detectfile)
-
     if args.append:
         try:
             fileh = tb.open_file(args.outfilename, 'a')
@@ -172,16 +285,23 @@ def main(argv=None):
     else:
         try:
             fileh = tb.open_file(args.outfilename, 'w')
-            fileh.create_group(fileh.root, 'Elixer', "Line Emission Classifications")
+            groupElix = fileh.create_group(fileh.root, 'Elixer', "ELiXer Summaries")
             fileh.create_table(fileh.root, 'Classifications', Classifications)
         except:
-            args.log.warning('Could not open %s.', args.detectfile)
+            args.log.warning('Could not open %s.', args.outfilename)
 
     table = fileh.root.Classifications
   
-    for detect in detect_list:
-        print detect
-
+    for detect_i in detect_list[0:50]:
+        
+        args.log.info('Ingesting detectid = %s' % detect_i)
+        try:
+            elixim = get_elixer_image(detect_i, args.elixer_path)
+            elixarray = fileh.create_array(groupElix, 'DEX' + str(detect_i), elixim)
+            elixarray.attrs['CLASS'] = 'IMAGE'
+        except:
+            args.log.warning('Could not open elixer image for %s' % detect_i)
+            
     table.flush()
     fileh.close()
 
