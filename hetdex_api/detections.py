c060665ccf054764af4245ed10996b9cb3a4ff08
eriniocentric <erin.jm.cooper@gmail.com>
2019-04-10 21:01:59 -0500

    got query functions working now

--- hetdex_api/detections.py
+++ hetdex_api/detections.py
@@ -17,7 +17,8 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 import pickle
 
-import config
+from hetdex_api.survey import Survey
+from hetdex_api import config
 
 
 class Detections:
@@ -37,6 +38,7 @@ class Detections:
             print('survey not in survey options')
             print(survey_options)
             return None
+
         self.filename = survey_options[survey]
         self.hdfile = tb.open_file(self.filename, mode='r')
         colnames = self.hdfile.root.Detections.colnames
@@ -56,7 +58,14 @@ class Detections:
             else:
                 setattr(self, name2,
                         getattr(self.hdfile_elix.root.Classifications.cols, name2)[:])
-                
+        
+        # also assign a field identifier
+        self.field = np.chararray(np.size(self.detectid),12)
+        S = Survey(survey)
+        for index, shot in enumerate(S.shotid):
+            ix = np.where(self.shotid == shot)
+            self.field[ix] = S.field[index]
+
 
     def query_by_coords(self, coords, radius):
         '''
@@ -66,14 +75,14 @@ class Detections:
         coords - astropy coordinate object
         radius - an astropy Quantity object, or a string 
         that can be parsed into one.  e.g., '1 degree' 
-        or 1*u.degree. Will assume arcsec by default
+        or 1*u.degree. Will assume arcsec if no units given
         
         '''
+        sep = self.coords.separation(coords) 
         try:
-            maskcoords = self.coords.separation(coords) < radius * u.degree
+            maskcoords = sep < radius
         except:
-            maskcoords = self.coords.separation(coords) < radius / 3600.
-            
+            maskcoords = sep.arcmin < radius
         return maskcoords
 
 
@@ -90,27 +99,27 @@ class Detections:
         if limits.wave_low or limits.wave_high: 
             maskwave = (self.wave > limits.wave_low) * (self.wave < limits.wave_high)
         else: 
-            maskwave = np.ones(np.size(ndets))
+            maskwave = np.ones(np.size(ndets), dtype=bool)
             
         if limits.flux_low or limits.flux_high:
             maskflux = (self.flux > limits.flux_low) * (self.flux < limits.flux_high)
         else:
-            maskflux = np.ones(np.size(ndets))
+            maskflux = np.ones(np.size(ndets), dtype=bool)
             
         if limits.linewidth_low or limits.linewidth_high:
             masklw = (self.linewidth > limits.linewidth_low) * (self.linewidth < limits.linewidth_high)
         else:
-            masklw = np.ones(np.size(ndets))
+            masklw = np.ones(np.size(ndets), dtype=bool)
             
         if limits.sn_low or limits.sn_high:
             masksn = (self.sn > limits.sn_low) * (self.sn < limits.sn_high)
         else:
-            masksn = np.ones(np.size(ndets))
+            masksn = np.ones(np.size(ndets), dtype=bool)
             
         if limits.chi2_low or limits.chi2_high:
             maskchi2 = (self.chi2 > limits.chi2_low) * (self.chi2 < limits.chi2_high)
         else:
-            maskchi2 = np.ones(np.size(ndets))
+            maskchi2 = np.ones(np.size(ndets), dtype=bool)
             
             
         if limits.aperture_flag:
@@ -118,10 +127,14 @@ class Detections:
             maskfield = self.query_by_coords(coords, limits.rad)
         else:
             try:
-                maskfield = np.ones(np.size(ndets))
+                maskfield = np.ones(np.size(ndets), dtype=bool)
                 
-                if limits.field != 'all':
-                    print('Subselecting for field(s):', limits.field)
+                if any(limits.field) != 'all':
+                    maskfield = np.zeros(np.size(ndets), dtype=bool)
+                    print 'Subselecting for field(s):', limits.field
+                    for field_index in limits.field:
+                        mask_i = (self.field == field_index)
+                        maskfield = maskfield | mask_i
             except:
                 print('Error downselecting based on field selection')
 
@@ -143,4 +156,19 @@ class Detections:
         
         '''
         limits = pickle.load( open( picklefile, "rb" ) )
-        query_by_dictionary(self, limits)
+        mask = self.query_by_dictionary(limits)
+        return mask
+
+        
+    def remove_bad_detects(self):
+        '''
+        Reads in the bad detect list from config.py
+        and removes those detectids
+        '''
+
+
+    def remove_shots(self, shotlist):
+        '''
+        Takes a list of shots and removes them
+        ''
+
