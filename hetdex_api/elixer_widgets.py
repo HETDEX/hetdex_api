b748517860f1214df111e283f5bccf3371e19137
eriniocentric <erin.jm.cooper@gmail.com>
2019-04-17 19:54:46 -0500

    initial devs

--- hetdex_api/elixer_widgets.py
+++ hetdex_api/elixer_widgets.py
@@ -1,26 +1,25 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import os.path as op
 import ipywidgets as widgets
 from IPython.display import Image
 from ipywidgets import interact, interactive
+from hetdex_api.detections import *
 
-
-# set up classificaiton dictionary
-class class_dict:
+# set up classificaiton dictionary                                                                     
+class classification_info:
     def __init__(self, detectlist):
         self.detectid = detectlist
-#        self.vis_class = -1*np.ones(np.size(detectlist))
         self.vis_class = np.chararray(np.size(detectlist), 15)
         self.comment = np.chararray(np.size(detectlist), 60)
     def add(self, detectid_i, vis_class_i, comment_i):
         ix = np.where(self.detectid == detectid_i)
         self.vis_class[ix] = vis_class_i
         self.comment[ix] = comment_i
+        
 
-
-
-def main(x):
+def main_display(x):
     detectid = detectbox.value
     elix_dir = '/scratch/03261/polonius/jpgs'
     file_jpg = op.join(elix_dir, "egs_%d" %(detectid//100000), str(detectid) + '.jpg')
@@ -31,9 +30,15 @@ def main(x):
     submitbutton.on_click(on_button_click)
 
 def on_button_click(b):
-    class_dict.add(detectbox.value, classification.value, notes.value)
+    class_info.add(detectbox.value, classification.value, notes.value)
     detectbox.value +=1
 
+
+detects = Detections('hdr1')
+
+detectlist = detects.detectid
+
+class_info = classification_info(detectlist)
 
 detectbox = widgets.BoundedIntText(
     value=7,
@@ -50,7 +55,7 @@ classification = widgets.ToggleButtons(
     disabled=False,
     button_style='', # 'success', 'info', 'warning', 'danger' or ''
     tooltips=['Description of slow', 'Description of regular', 'Description of fast'],
-#     icons=['check'] * 3
+    #     icons=['check'] * 3
 )
 notes = widgets.Text(
     value='',
@@ -60,4 +65,4 @@ notes = widgets.Text(
 )
 submitbutton = widgets.Button(description="Select Detections", button_style='success')
 
-interact(main, x=detectbox)
+interact(main_display, x=detectbox)
