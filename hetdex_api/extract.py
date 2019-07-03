8a206224549871d4d59333a313c0f24c6b3f72ca
eriniocentric <erin.jm.cooper@gmail.com>
2019-07-03 08:51:52 -0500

    Changed import shot to import hetdex_api.shot

--- hetdex_api/extract.py
+++ hetdex_api/extract.py
@@ -13,7 +13,7 @@ from astropy.coordinates import SkyCoord
 from astropy.modeling.models import Moffat2D, Gaussian2D
 from astropy import units as u
 from scipy.interpolate import griddata, LinearNDInterpolator
-from shot import Fibers
+from hetdex_api.shot import Fibers
 import imp
 
 input_utils = imp.load_source('input_utils',
