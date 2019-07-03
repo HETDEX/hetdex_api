806654b452d48d58318ef9e693496263e3bde4c1
Jan Snigula <snigula@mpe.mpg.de>
2019-07-03 14:04:31 +0200

    Added AllAmplifierQC class

--- hetdex_api/qc.py
+++ hetdex_api/qc.py
@@ -1,5 +1,7 @@
 import json
 import os
+import copy
+import six
 
 try:
     # Python 3
@@ -33,7 +35,7 @@ class AmplifierQC():
 
         self._datestr = datestr
         self._qcdata = json.loads(resp.read())
-        self._pristine = True
+        self._original = copy.copy(self._qcdata)
 
         # Attempt to read the authorization key
         try:
@@ -89,14 +91,14 @@ class AmplifierQC():
                 self._qcdata[ifuslot] = {}
 
             self._qcdata[ifuslot][amp] = value
-            self._pristine = False
 
-    def save(self):
+    def save(self, force=False):
         '''
         Save the updated quality control data to the database.
         '''
 
-        if self._pristine:
+        if self._original == self._qcdata:
+            print('No changes found, not saving to database.')
             return
 
         if self._apikey == '':
@@ -106,13 +108,162 @@ class AmplifierQC():
         url = 'https://luna.mpe.mpg.de/qc/' + self._datestr
 
         try:
-            req = Request(url=url, data=json.dumps(self._qcdata).encode(),
+            datadict = {'new': self._qcdata,
+                        'old': self._original}
+            if force:
+                datadict['forceupdate'] = True
+            req = Request(url=url,
+                          data=json.dumps(datadict).encode(),
                           headers={'Authorization':
                                    'FPS_API apikey='+self._apikey},
                           method='PUT')
             urlopen(req)
         except HTTPError as e:
             raise Exception(' Failed to retrieve qc data, server '
+                            'responded with %d %s' % (e.getcode(), e.reason))
+
+    def set_apikey(self, key):
+        '''
+        Update the apikey, if it wasn't set in the HETDEX_APIKEY shell
+        environment variable.
+
+        Parameters
+        ----------
+        key : str
+            API key to use for saving the quality control data.
+
+        '''
+
+        self._apikey = key
+
+
+class AllAmplifierQC():
+
+    def __init__(self):
+        '''
+        Initialize the AmplifierQC object for a given date
+
+        Parameters
+        ----------
+        datestr : str
+            The date string to check, must be of format YYYYMMDD,
+            otherwise the server will return an error.
+        '''
+
+        url = 'https://luna.mpe.mpg.de/qc/all'
+
+        try:
+            resp = urlopen(url)
+        except HTTPError as e:
+            raise Exception(' Failed to retrieve qc data, server '
+                            'responded with %d %s' % (e.getcode(), e.reason))
+
+        self._qcdata = json.loads(resp.read())
+        self._original = copy.copy(self._qcdata)
+
+        # Attempt to read the authorization key
+        try:
+            self._apikey = os.environ['HETDEX_APIKEY']
+        except KeyError:
+            self._apikey = ''
+
+    def get_amp(self, date, ifuslot, amp):
+        '''
+        Retrieve the quality control information for a given IFUslot and
+        amplifier.
+
+        Parameters
+        ----------
+        date : int or str
+            Date for the qc data
+        ifuslot : str
+            The IFUslot to check e.g. '094' or '113'
+        amp : str
+            The amplifier to check one of. 'LL', 'RL', 'LU'. 'RU'
+
+        Returns
+        -------
+        The stored error code for this ifuslot / amplifier combination, 0
+        otherwise.
+
+        '''
+        if isinstance(date, six.string_types):
+            date = int(date)
+
+        if date not in self._qcdata:
+            print('WARNING! No data for date %d' % date)
+            return 0
+        try:
+            return self._qcdata[ifuslot][amp]
+        except KeyError:
+            return 0
+
+    def set_amp(self, date, ifuslot, amp, value):
+        '''
+        Update the locally stored quality control data for the given
+        IFUslot and amplifier.
+
+        Parameters
+        ----------
+        date : int or str
+            Date for the qc data
+        ifuslot : str
+            The IFUslot to check e.g. '094' or '113'
+        amp : str
+            The amplifier to check one of. 'LL', 'RL', 'LU'. 'RU' and 'ALL'
+            to set all amplifiers at once
+        value : int
+            The error value for this amplifier.
+        '''
+        if isinstance(date, six.string_types):
+            date = int(date)
+
+        if date not in self._qcdata:
+            self._qcdata[date] = {}
+        if amp == 'ALL':
+            for a in ['LL', 'RL', 'RU', 'LU']:
+                self.set_amp(date, ifuslot, a, value)
+        else:
+            if ifuslot not in self._qcdata[date]:
+                self._qcdata[date][ifuslot] = {}
+
+            self._qcdata[date][ifuslot][amp] = value
+
+    def save(self, force=False):
+        '''
+        Save the updated quality control data to the database.
+        '''
+
+        if self._original == self._qcdata:
+            print('No changes found, not saving to database.')
+            return
+
+        if self._apikey == '':
+            raise Exception('Cannot save to database without api key. Please'
+                            ' set \'HETDEX_APIKEY\' in the shell, or call'
+                            'set_apikey.')
+        url = 'https://luna.mpe.mpg.de/qc/'
+
+        try:
+            for d in self._qcdata.keys():
+                if d not in self._original:
+                    self._original[d] = {}
+
+                if self._qcdata[d] == self._original[d]:
+                    continue
+
+                datadict = {'new': self._qcdata[d],
+                            'old': self._original[d]}
+                if force:
+                    datadict['forceupdate'] = True
+                req = Request(url=url + str(d),
+                              data=json.dumps(datadict).encode(),
+                              headers={'Authorization':
+                                       'FPS_API apikey='+self._apikey},
+                              method='PUT')
+            urlopen(req)
+        except HTTPError as e:
+            raise Exception(' Failed to save qc data, server '
                             'responded with %d %s' % (e.getcode(), e.reason))
 
     def set_apikey(self, key):
