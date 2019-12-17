32a9d7b4e146f9a7636b16e65290936b92a24d14
eriniocentric <erin.jm.cooper@gmail.com>
2019-12-16 20:40:43 -0600

    added setuptools to mock list

--- docs/conf.py
+++ docs/conf.py
@@ -63,4 +63,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autodoc_mock_imports = ['astropy', 'tables', 'ipywidgets']
+autodoc_mock_imports = ['astropy', 'tables', 'ipywidgets', 'setuptools']
