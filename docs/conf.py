3865a51b4258602020b4823fd1934acdf3ea84d9
eriniocentric <erin.jm.cooper@gmail.com>
2019-12-19 16:11:00 -0600

    made astroquery a mock

--- docs/conf.py
+++ docs/conf.py
@@ -62,4 +62,5 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
 
-autodoc_mock_imports = ['astropy', 'tables', 'ipywidgets', 'elixer']
+autodoc_mock_imports = ['astropy', 'tables', 'ipywidgets', 'elixer',
+                        'astroquery']
