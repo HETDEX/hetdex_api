96bae7c773f05a1170899653352c7eef99947565
eriniocentric <erin.jm.cooper@gmail.com>
2020-01-09 14:06:02 -0600

    Added requirements for query_widget

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 install_requires = ['numpy', 'astropy>=1.2, !=1.3.3', 'scipy>=0.17.0',
-                    'tables', 'ipywidgets', 'speclite']
+                    'tables', 'ipywidgets', 'speclite', 'astrowidgets',
+                    'jsonschema==3.1.1', 'ipyevents']
 
 extras = {}
 
@@ -44,8 +45,9 @@ setup(
                         'biweight_fluxlims_hdf5 = hetdex_api.flux_limits.collapse_cubes:return_biwt_cmd',
                         'add_sensitivity_cube_to_hdf5 =  hetdex_api.flux_limits.hdf5_sensitivity_cubes:add_sensitivity_cube_to_hdf5',
                         'extract_sensitivity_cube = hetdex_api.flux_limits.hdf5_sensitivity_cubes:extract_sensitivity_cube',
-                        'hetdex_get_spec = hetdex_tools.get_spec',
-                        'hetdex_get_spec2D = hetdex_tools.get_spec2D'
+                        'hetdex_get_spec = hetdex_tools.get_spec:main',
+                        'hetdex_get_spec2D = hetdex_tools.get_spec2D:main',
+                        'hetdex_get_shots = hetdex_tools.get_shots_of_interest:main'
                      ]
                    },
 
