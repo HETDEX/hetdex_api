Fixed wrong sign

hetdex_api/elixer_widget_cls.py
@@ -517,19 +517,18 @@ class ElixerWidget():
 
     def goto_previous_detect(self):
 
-        if True:#try:
+        try:
             if self.detectbox.value in self.detectid:
                 ix = np.where(self.detectid == self.detectbox.value)[0][0]
                 ix -= 1
             else:
                 ix = np.max(np.where(self.detectid <= self.detectbox.value))
 
-            print(ix)
-            if ix > 0:
+            if ix < 0:
                 ix = 0
                 print("At the beginning of the DetectID List")
                 return
-        else:#except:
+        except:
             #invalid index ... the report displayed is not in the operating list
             #so use the last good index:
             ix = self.current_idx