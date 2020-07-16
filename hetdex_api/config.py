55b300153b02bc8f5bdf6a9573c36761605be775
eriniocentric <erin.jm.cooper@gmail.com>
2020-07-16 08:34:27 -0500

    Adding bad amps path to fits flag file

--- hetdex_api/config.py
+++ hetdex_api/config.py
@@ -77,8 +77,8 @@ class HDRconfig:
         if survey == 'hdr2.1':
             self.bad_dir = "/work/05350/ecooper/wrangler/hetdex_api/known_issues/hdr2.1"
             self.baddetect = op.join(self.bad_dir, "baddetects.list")
-            self.badshot = op.join(self.bad_dir, "badshots.list")
-            self.badamp = op.join(self.bad_dir, "badamps.list")
+            self.badshot = op.join(self.hdr_dir[survey], "survey", "amp_flag.fits")
+            self.badamp = op.join(self.bad_dir, "badamps.fits")
             self.badpix = op.join(self.bad_dir, "badpix.list")
             self.elix_dir = "/data/03261/polonius/hdr2/detect/image_db"
             self.imaging_dir = "/data/03261/polonius/hdr2/imaging"
