fc750f82ab47c0590aff9d33b8fdf2878cfaae69
Daniel Farrow <dfarrow@login1.wrangler.tacc.utexas.edu>
2020-07-28 05:35:10 -0500

    Added bash scripts used to generate HDR2.1 flux limits

---
+++ bash_scripts/hdr2.1/generate_sensitivity_cube_hdf5s.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+#
+# Generate the HDF5 sensitivity cubes from
+# the flux limit FITs files. Also compute
+# the averages across a shot
+# 
+#  Arguments:
+#  $1  = run number to consider
+#
+# Daniel Farrow (MPE)
+#
+
+# Script to compute averages
+AV_FLIM_SCRIPT=/home/04120/dfarrow/HETDEX_API_devel/hetdex_tools/compute_average_one_sigma.py 
+
+# List of datevshots
+DATE_FN=shotlist
+redo="false"
+
+# input and output paths
+INPATH=/data/04120/dfarrow/hdr2.1_fits_cubes/run$1
+OUTPATH=/data/04120/dfarrow/hdr2.1_hdfs
+
+# File in to which to write the biweight location of the flux limits at 4540AA for each shot
+AVERAGE_FLIM_FILE=$OUTPATH/average_one_sigma_run$1.txt.fixbadifu
+
+cd $INPATH
+
+for datevobs in `cat $DATE_FN`
+do
+ 
+    echo $datevobs
+
+    if [ -f ${OUTPATH}/${datevobs}_sensitivity_cube.h5 ]; then
+        if [ "$redo" == "true" ]
+        then
+            mv ${OUTPATH}/${datevobs}_sensitivity_cube.h5 ${OUTPATH}/${datevobs}_sensitivity_cube.h5.old
+        else
+            echo "${OUTPATH}/${datevobs}_sensitivity_cube.h5 exists, skipping"
+            continue
+        fi
+    fi
+  
+    files=`ls ${datevobs}_???_???_???_flim.fits`
+    echo $files
+    python $AV_FLIM_SCRIPT --fn-shot-average $AVERAGE_FLIM_FILE $files
+    add_sensitivity_cube_to_hdf5 $files "${OUTPATH}/${datevobs}_sensitivity_cube.h5"
+done
---
+++ bash_scripts/hdr2.1/verify_flux_cube_runs.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# Verify that the flux limit cubes
+# ran and prodcued output of non-zero
+# size
+
+
+#for run in $(seq 1 17)
+for run in 17
+do
+   pushd run$run
+   cat rflim | while read line
+   do
+     ifu=`echo $line | awk '{print $7}'` 
+     datevshot=`echo $line | awk '{print $8}'`
+     fn="${datevshot}_${ifu}_flim.fits"
+
+     # Find file size and save return value
+     duo=`du $fn`
+     rval=$?
+     size=`echo $duo | awk '{print $1}'` 
+
+     if [ "$rval" != "0" ] || [ "$size" == "0" ]; then
+         echo "pushd run${run} ; ${line} ; popd" >> ../torerun
+         echo $fn >> ../failures
+     fi
+   done
+   popd
+done
