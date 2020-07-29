31681f1766ead5e1578838c31f97993f1598de55
eriniocentric <erin.jm.cooper@gmail.com>
2020-07-29 07:48:31 -0500

    Adding flux limit in API for hdr2.1

--- hetdex_api/survey.py
+++ hetdex_api/survey.py
@@ -44,6 +44,7 @@ class Survey:
         self.filename = config.surveyh5
         self.hdfile = tb.open_file(self.filename, mode="r")
         colnames = self.hdfile.root.Survey.colnames
+
         for name in colnames:
             if name == "ra_flag":
                 setattr(
@@ -63,6 +64,17 @@ class Survey:
         # set the SkyCoords
         self.coords = SkyCoord(self.ra * u.degree, self.dec * u.degree, frame="icrs")
 
+        # append flux limits
+        if args.survey == 'hdr2.1':
+            
+            flim = Table.read('/data/05350/ecooper/hdr2.1/survey/average_one_sigma.txt',
+                              format='ascii', names=['datevobs', 'fluxlimit_4550'])
+            fluxlimit = []
+            for datevobs in self.datevobs:
+                sel = flim['datevobs'] == datevobs
+                fluxlimit.append( flim['fluxlimit_4550'] )
+            self.fluxlimit_4550 = np.array(fluxlimit)
+                              
     def __getitem__(self, indx):
         """ 
         This allows for slicing of the survey class
