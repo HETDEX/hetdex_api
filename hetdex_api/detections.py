745aaf5f46ba4358efd8885816e549401d1edc39
eriniocentric <erin.jm.cooper@gmail.com>
2020-08-01 06:54:07 -0500

    moving slice into initialization

--- hetdex_api/detections.py
+++ hetdex_api/detections.py
@@ -221,8 +221,15 @@ class Detections:
 
         if self.survey == 'hdr2.1':
             baddetects = pickle.load( open( config.baddetectmask, "rb"))
-            self = self[baddetects]
-                        
+            p = copy.copy(self)
+            attrnames = self.__dict__.keys()
+            for attrname in attrnames:
+                try:
+                    setattr(p, attrname, getattr(self, attrname)[baddetects])
+                except:
+                    setattr(p, attrname, getattr(self, attrname))
+            self = p
+            
             
     def __getitem__(self, indx):
         """ 
