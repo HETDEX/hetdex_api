2b398cb1b9760f7dcd4d6330e06192597991db86
eriniocentric <erin.jm.cooper@gmail.com>
2020-09-08 16:50:04 -0500

    Added meteor flag function

--- hetdex_api/mask.py
+++ hetdex_api/mask.py
@@ -32,11 +32,36 @@ def amp_flag_from_coords(coords, FibIndex, bad_amps_table, radius=3.*u.arcsec, s
         an astropy.coordinates SkyCoord object
     FibIndex
         a hetdex_api.survey FiberIndex class object
+    bad_amps_table
+        astropy table containing the bad amp flag values. This can
+        be retrieved from config.badamp
     radius
         radius to search for fibers
     shotid
         shotid to search. If none it will search all shots at once. If
         any are flagged bad then it will return False for all.
+
+    Returns
+    -------
+    None if no matching fiber is found in the aperture
+
+    True if all fibers in aperture are on good amps
+
+    False if any fiber in the defined region is flagged in a bad amp
+
+    Examples
+    --------
+    from hetdex_api.config import HDRconfig
+    from hetdex_api.mask import *
+
+    config = HDRconfig()
+    bad_amps_table = Table.read(config.badamp)
+    FibIndex = FiberIndex()
+
+    coords = SkyCoord(11.628530 * u.deg, 0.081790 * u.deg, frame='icrs')
+
+    flag = amp_flag_from_coords(coords, FibIndex,  bad_amps_table,
+                                radius=1.5*u.arcsec, shotid=20181003009)
     
     """
 
@@ -64,9 +89,82 @@ def amp_flag_from_coords(coords, FibIndex, bad_amps_table, radius=3.*u.arcsec, s
     return amp_flag
 
 def amp_flag_from_fiberid(fiberid, bad_amps_table):
+    """
+    Returns a boolean flag whether the amp has been flagged usable
+    
+    Parameters
+    ----------
+    fiberid
+        string with the unique fiber_id info
+    
+    bad_amps_table
+        astropy table containing the bad amp flag values. This can
+        be retrieved from config.badamp  
+
+    Returns
+    -------
+    None if no matching fiber is found to the coords
+    
+    True if amp is usable
+    
+    False if any fiber in the defined region is flagged in a bad amp
+    
+    """
+
     shotid = int(fiberid[0:11])
     mf = fiberid[14:34]
     
     sel = (bad_amps_table['shotid'] == shotid) * (bad_amps_table['multiframe'] == mf)
 
     return bad_amps_table['flag'][sel][0]
+
+    
+def meteor_flag_from_coords(coords, shotid=None):
+    """
+    Returns a boolean flag value to mask out meteors
+
+    Parameters
+    ----------
+    coords
+        an astropy.coordinates SkyCoord object
+    shotid
+        shotid to search. If none it will search all shots at once. If
+        any are flagged bad then it will return False for all.
+
+    Returns
+    -------
+    bool
+        True if no meteors fall in the aperture
+        False if a meteor falls in the aperture
+
+    Example
+    -------
+    
+    """
+
+    global config
+
+    # meteors are found with +/- 8 arcsec of the line DEC=a+RA*b in this file
+    met_tab = Table.read('/work/05350/ecooper/wrangler/mask/meteor.list', format='ascii')
+    sel_shot = met_tab['shotid'] == shotid
+
+    if np.size(sel_shot) > 0:
+        a = met_tab['a'][sel_shot]
+        b = met_tab['b'][sel_shot]
+
+        ra_met = coords.ra + np.arange(-90,90)*u.arcsec
+        dec_met = a + ra_met.deg*b
+        
+        met_coords = SkyCoord(ra=coords.ra, dec=dec_met*u.deg)
+
+        meteor_match = coords.separation(met_coords) < 8.*u.arcsec
+
+        if np.any(meteor_match):
+            flag = False
+        else:
+            flag = True
+    else:
+        flag = True
+
+    return flag
+    
