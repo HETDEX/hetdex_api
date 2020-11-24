fd41a45e88b2cfaac59cab4e789b2802ae1d3ef6
eriniocentric <erin.jm.cooper@gmail.com>
2020-11-23 18:33:39 -0600

    generalizing the cube dims

--- hetdex_tools/interpolate.py
+++ hetdex_tools/interpolate.py
@@ -191,6 +191,7 @@ def make_data_cube(
     imsize=30.0 * u.arcsec,
     wave_range=[3470, 5540],
     dwave=2.0,
+    dcont=50.0,
     convolve_image=True,
     ffsky=True,
     subcont=False,
@@ -203,33 +204,36 @@ def make_data_cube(
     Paramaters
     ----------
     detectid: int
-                detectid from the continuum or lines catalog. Default is
-                None. Provide a coords/shotid combo if this isn't given
+        detectid from the continuum or lines catalog. Default is
+        None. Provide a coords/shotid combo if this isn't given
     coords: SkyCoords object
-                coordinates to define the centre of the data cube
+        coordinates to define the centre of the data cube
     pixscale: astropy angle quantity
-                plate scale
+        plate scale
     imsize: astropy angle quantity
-                spatial length of cube (equal dims is only option)
+        spatial length of cube (equal dims is only option)
     wave_range: list
-                start and stop value for the wavelength range in Angstrom
+        start and stop value for the wavelength range in Angstrom
     dwave
-                step in wavelength range in Angstrom
+        step in wavelength range in Angstrom
     convolve_image: bool
-                option to convolve image with shotid seeing
+         option to convolve image with shotid seeing
     ffsky: bool
-                option to use full frame calibrated fibers. Default is
-                True.
+        option to use full frame calibrated fibers. Default is
+        True.
     subcont: bool
         option to subtract continuum. Default is False. This
         will measure the continuum 50AA below and above the
-        input wave_range 
+        input wave_range
+    dcont
+        width in angstrom to measure the continuum. Default is to
+        measure 50 AA wide regions on either side of the line  
 
     Returns
     -------
     hdu: PrimaryHDU object
         the data cube 3D array and associated 3d header
-        Units are '10^-17 erg cm-2 s-1'
+        Units are '10^-17 erg cm-2 s-1 per spaxel'
 
     Examples
     --------
@@ -327,7 +331,7 @@ def make_data_cube(
                     scale=pixscale.to(u.arcsec).value,
                     boxsize=imsize.to(u.arcsec).value,
                     nchunks=2,
-                    wrange=[wave_i-50, wave_i],
+                    wrange=[wave_i-dcont, wave_i],
                     convolve_image=convolve_image,
                 )
                 zarray_red = E.make_narrowband_image(
@@ -341,13 +345,16 @@ def make_data_cube(
                     nchunks=2,
                     scale=pixscale.to(u.arcsec).value,
                     boxsize=imsize.to(u.arcsec).value,
-                    wrange=[wave_i + dwave, wave_i + dwave + 50],
+                    wrange=[wave_i + dwave, wave_i + dwave + dcont],
                     convolve_image=convolve_image,
                 )
 
-                im_cont = (zarray_blue[0] + zarray_red[0])/(50)
-                im_slice = im_src[0] - im_cont
-
+# this works
+#                im_cont = (zarray_blue[0] + zarray_red[0])/(50)
+#                im_slice = im_src[0] - im_cont
+                im_cont = (zarray_blue[0] + zarray_red[0])/(2*dcont)
+                im_slice = im_src[0] - dwave*im_cont
+                
             im_cube[i, :, :] = im_slice
 
         except Exception:
