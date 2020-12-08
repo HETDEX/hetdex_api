d83edd2240e712cf3d52071fbf6ad7cb1b0f6079
eriniocentric <erin.jm.cooper@gmail.com>
2020-12-08 12:34:49 -0600

    gal_region-> galregion

--- hetdex_api/detections.py
+++ hetdex_api/detections.py
@@ -761,8 +761,11 @@ class Detections:
             rlimit = 1.1*d25scale*row["SemiMajorAxis"]*u.arcmin
 
             if np.any(self.coords.separation(gal_coord) < rlimit):
-                galregion = create_gal_ellipse(galaxy_cat, row_index=idx, d25scale=d25scale)
-                dummy_wcs = create_dummy_wcs(gal_region.center, imsize=2*gal_region.height)
+                galregion = create_gal_ellipse(galaxy_cat,
+                                               row_index=idx,
+                                               d25scale=d25scale)
+                dummy_wcs = create_dummy_wcs(galregion.center,
+                                             imsize=2*galregion.height)
                 galflag = galregion.contains(self.coords, dummy_wcs)
                 mask = mask * np.invert(galflag)
 
