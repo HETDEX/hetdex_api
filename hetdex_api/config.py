730c0d050d412b17f1ba30e8bd2cfeeca2377989
eriniocentric <erin.jm.cooper@gmail.com>
2021-01-25 10:58:29 -0600

    Changed imaging_dir and add /work/03946 to list of host_dir options

--- hetdex_api/config.py
+++ hetdex_api/config.py
@@ -17,10 +17,12 @@ class HDRconfig:
             self.host_dir = "/home/jovyan/Hobby-Eberly-Telesco"
         elif op.exists("/home/idies/workspace/HETDEX"):
             self.host_dir = "/home/idies/workspace/HETDEX"
-        elif op.exists("/data/05350/ecooper"):
-            self.host_dir = "/data/05350/ecooper"
+        #elif op.exists("/data/05350/ecooper"):
+        #    self.host_dir = "/data/05350/ecooper"
         elif op.exists("/scratch/03946/hetdex"):
             self.host_dir = "/scratch/03946/hetdex"
+        elif op.exists("/work/03946/hetdex"):
+            self.host_dir = "/work/03946/hetdex"
         elif op.exists("/data/hetdex/u/dfarrow/hetdex_data"):
             self.host_dir = "/data/hetdex/u/dfarrow/hetdex_data"
         else:
@@ -74,7 +76,7 @@ class HDRconfig:
         except:
             pass
         self.elixerh5 = op.join(self.hdr_dir[survey], "detect", "elixer.h5")
-        self.imaging_dir = op.join(self.hdr_dir[survey], "imaging")
+        self.imaging_dir = op.join(self.host_dir, "imaging")
         self.contsourceh5 = op.join(
             self.hdr_dir[survey], "detect", "continuum_sources.h5"
         )
