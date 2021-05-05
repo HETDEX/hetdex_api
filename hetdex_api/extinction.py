31ee38d1b325d9b046f12ba649f793c5d1ebb6c8
eriniocentric <erin.jm.cooper@gmail.com>
2021-05-05 14:04:04 -0500

    add some extinction tools

---
+++ hetdex_api/extinction.py
@@ -0,0 +1,80 @@
+"""
+Scripts to deal with extinction.
+
+This includes HDR2.1 flat extinction fix
+
+"""
+
+import numpy as np
+from scipy import interpolate
+import extinction
+from dustmaps.sfd import SFDQuery
+from hetdex_api.config import HDRconfig
+
+
+def dustmaps_setup():
+    # this is how I intially set up dustmaps
+    # Need to put maps in config.dustmaps
+
+    from dustmaps.config import config as dustmaps_config
+    import dustmaps
+    config = HDRconfig()
+    dustmaps_config['data_dir'] = config.dustmaps
+    dustmaps.sfd.fetch()
+
+
+def get_2pt1_extinction_fix(pad=True):
+    """
+    This is to fix the E(B-V)=0.02
+    flat extinction applied to 2.1
+
+    Curve from Karl
+    /work/00115/gebhardt/fluxcor/extinction
+
+    Paramaters
+    ----------
+    pad  bool
+    This is to pad curve to 3470 and 5400 to match
+    wave_rect
+    """
+    config = HDRconfig()
+
+    karl_data = np.loadtxt( config.extinction_fix)
+
+    if pad:
+        karl_wave = np.concatenate(([3450.],
+                                    karl_data[:, 0],
+                                    [5550.]))
+        karl_curve = np.concatenate(([karl_data[0, 1]],
+                                     karl_data[:, 1],
+                                     [karl_data[-1, 1]]))
+    else:
+        karl_wave = karl_data[:, 0]
+        karl_curve = karl_data[:, 1]
+
+    correction = interpolate.interp1d( karl_wave, karl_curve)
+
+    return correction
+
+
+def deredden_spectra(wave, coords):
+    """
+    Apply S&F 2011 Extinction from SFD Map
+    https://iopscience.iop.org/article/10.1088/0004-637X/737/2/103#apj398709t6
+
+    Paramters
+    ---------
+    wave      array
+        wavelength to apply correction
+    coords    SkyCoord object
+        sky coordinates
+    """
+    Rv = 2.742  # Landolt V
+    sfd = SFDQuery()
+    ebv = sfd(coords)
+    Av = Rv*ebv
+    ext = extinction.fitzpatrick99(wave, Av, Rv)
+
+    deredden = 10**(0.4*np.array(ext))
+
+    return deredden
