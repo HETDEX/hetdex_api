a6700810c1bba74180e33df84e28ddd2f40c12db
eriniocentric <erin.jm.cooper@gmail.com>
2021-06-27 09:26:49 -0500

    add quantity to radius in get_fibers_table call

--- hetdex_api/extract.py
+++ hetdex_api/extract.py
@@ -218,7 +218,7 @@ class Extract:
         else:
 
             fib_table = get_fibers_table(
-                self.shot, coord, survey=self.survey, radius=radius
+                self.shot, coord, survey=self.survey, radius=radius*u.arcsec,
             )
 
             if np.size(fib_table) < fiber_lower_limit:
