1177e044b494e2bc61942799ab2c0a6d8eebf8bd
eriniocentric <erin.jm.cooper@gmail.com>
2022-09-29 15:06:17 -0500

    test

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','config.py']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','get_shots_of_interest']
 
 
 # -- Options for HTML output -------------------------------------------------
