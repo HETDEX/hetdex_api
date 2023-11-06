da2c59fb7432c52c0bd3402fd19b1d89534f166f
eriniocentric <erin.jm.cooper@gmail.com>
2023-11-06 12:18:10 -0600

    added pdr1 line

--- hetdex_api/config.py
+++ hetdex_api/config.py
@@ -34,6 +34,7 @@ class HDRconfig:
             "hdr2.1": op.join(self.host_dir, "hdr2.1"),
             "hdr3": op.join(self.host_dir, "hdr3"),
             "hdr4": op.join(self.host_dir, "hdr4"),
+            "pdr1": op.join(self.host_dir, "pdr1"),
         }
 
         self.survey_dir = op.join(self.hdr_dir[survey], "survey")
