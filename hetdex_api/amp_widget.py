e70ef0ff509a2f93aaf7db89be050829c874203b
eriniocentric <erin.jm.cooper@gmail.com>
2023-11-06 12:18:40 -0600

    added hdr4 options

--- hetdex_api/amp_widget.py
+++ hetdex_api/amp_widget.py
@@ -40,7 +40,7 @@ class AmpWidget:
         survey=LATEST_HDR_NAME,
         coords=None,
         radius=3.0,
-        detectid=2100000000,
+        detectid=None,
         wave=None,
         shotid=None,
         multiframe=None,
@@ -49,9 +49,18 @@ class AmpWidget:
     ):
 
         self.survey = survey.lower()
+        
         self.detectid = detectid
         self.shotid = shotid
         self.multiframe = multiframe
+
+        if self.shotid is None:
+            if self.multiframe is None:
+                if self.detectid is None:
+                    self.detectid = 4013993285 #random default LAE
+            else:
+                print('You must provide a shotid/multiframe combo or a detectid')
+                
         self.imtype = imtype
         self.expnum = expnum
         self.coords = coords
@@ -71,18 +80,10 @@ class AmpWidget:
         # initialize the image widget from astrowidgets
         self.imw = ImageWidget()  # image_width=600, image_height=600)
 
-        #        self.month_widget = widgets.Dropdown(
-        #            options=monthlist,
-        #            value=202001,
-        #            description='Month',
-        #            disabled=False,
-        #            layout=Layout(width='20%'),
-        #        )
-
         self.detectbox = widgets.BoundedIntText(
             value=self.detectid,
             min=2100000000,
-            max=5000000000,
+            max=6000000000,
             step=1,
             description="DetectID:",
             disabled=False,
@@ -205,7 +206,7 @@ class AmpWidget:
         self.bottombox = widgets.Output(layout={"border": "1px solid black"})
         
         if self.coords is not None:
-            if self.detectid is not None:
+            if self.detectid is None:
                 
                 self.shotid = None
                 
@@ -369,7 +370,11 @@ class AmpWidget:
         elif (self.detectid >= 3000000000) * (self.detectid < 3090000000):
             self.det_file = CONFIG_HDR3.detecth5
         elif (self.detectid >= 3090000000) * (self.detectid < 3100000000):
-            self.det_file = CONFIG_HDR2.contsourceh5
+            self.det_file = CONFIG_HDR3.contsourceh5
+        elif (self.detectid >= 4000000000) * (self.detectid < 4090000000):
+            self.det_file = CONFIG_HDR4.detecth5
+        elif (self.detectid >= 4090000000) * (self.detectid < 4100000000):
+            self.det_file = CONFIG_HDR4.contsourceh5
 
         if OPEN_DET_FILE is None:
 
