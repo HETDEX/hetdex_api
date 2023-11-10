68b1c01fa898a60b13ddd4e3d1558e6d6bfa9aab
eriniocentric <erin.jm.cooper@gmail.com>
2023-11-10 14:37:12 -0600

    update

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','get_shots_of_interest']
 #html_theme = 'alabaster'
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
