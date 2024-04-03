b20425601414c9e079588971ed457a20071f831b
eriniocentric <erin.jm.cooper@gmail.com>
2024-04-03 06:26:54 -0500

    add autodoc extensions

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,11 @@ release = '0.8'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
