8aa223bd8c3f78933432cf02ff2097a5861295ff
eriniocentric <erin.jm.cooper@gmail.com>
2024-04-03 08:29:46 -0500

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -41,5 +42,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
