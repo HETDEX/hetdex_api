66b9fb8d62cb9bde41953412197a68870826ccb3
eriniocentric <erin.jm.cooper@gmail.com>
2024-04-03 06:17:02 -0500

    remove mst parser

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.8'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
