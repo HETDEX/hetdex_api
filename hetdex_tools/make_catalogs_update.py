014930a692ca9391aa57a58e649f66d6a201944b
eriniocentric <erin.jm..cooper@gmail.com>
2026-03-26 17:55:35 -0500

    Add new CNN values to 5.0.2 catalog

--- hetdex_tools/make_catalogs_update.py
+++ hetdex_tools/make_catalogs_update.py
@@ -7,11 +7,61 @@ from astropy.table import Table, join, vstack
 import tables as tb
 import joblib
 
+from astropy.cosmology import Planck18 as cosmo
+
+
 version = '5.0.2'
+
+BADVAL = -999.0
 
 update_det_flags = True
 
 source_table = Table.read("source_catalog_{}.yy.fits".format(version))
+
+# add in apcor for continuum sources
+
+elixh5 = tb.open_file('/scratch/projects/hetdex/hdr5/detect/elixer_hdr345_cluster_cat.h5', 'r')
+
+elix_cont_tab = Table( elixh5.root.Detections.read())
+
+print('length of source table before apcor fix', len(source_table))
+
+tmp = join(source_table, elix_cont_tab['detectid', 'apcor_4500'],
+           keys='detectid', join_type='left')
+
+mask = source_table['apcor'] == 0
+source_table['apcor'][mask] = tmp['apcor_4500'][mask]
+print('length of source table after apcor fix', len(source_table))
+
+# fix some bad OII aper fluxes
+
+sel_bad_aper = ( (source_table['flux_aper']> 1e6) | ( source_table['flux_aper'] < 0) ) 
+source_table['flux_aper'][sel_bad_aper] = BADVAL
+source_table['flux_aper_err'][sel_bad_aper] = BADVAL
+
+# then fix the good oiis to use pipeline flux for these
+
+
+sel_oii_update = sel_bad_aper & (source_table['source_type'] == 'oii') & (source_table['flag_aper'] == 1)
+
+print( 'Updating flux and lum values for {} OIIs'.format(np.sum(sel_oii_update)))
+
+source_table['flux_oii'][sel_oii_update] = source_table['flux'][sel_oii_update]
+source_table['flux_oii_err'][sel_oii_update] = source_table['flux_err'][sel_oii_update]
+source_table['flag_aper'][sel_oii_update] = 1
+
+lum_dist = cosmo.luminosity_distance(source_table["z_hetdex"][sel_oii_update]).to(u.cm)
+fac = 10 ** (-17) * 4.0 * np.pi * lum_dist**2
+
+source_table["lum_oii"][sel_oii_update] = source_table["flux_oii"][sel_oii_update] * fac
+source_table["lum_oii_err"][sel_oii_update] = source_table["flux_oii_err"][sel_oii_update] * fac
+
+# force other weird negative flux measurements to same BADVAL
+
+for col in ['flux_aper', 'flux_oii', 'flux_oii_err', 'lum_oii', 'lum_oii_err', 'lum_lya', 'lum_lya_err']:
+    sel = (source_table[col] < 0) & (source_table[col] > BADVAL)
+    print(col, np.sum(sel))
+    source_table[col][sel] = BADVAL
 
 # add in the bad qso dets
 
@@ -107,14 +157,18 @@ source_table['p_conf'][sel_sample] = p_conf[sel_sample]
 
 # add in Shiro's CNN Score Values
 cnn_score_lae = Table.read(
-    '/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_5.0.3_lae.txt', # updated 2025-09-26, matches Karls
+    '/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_lae_20260316.txt', #updated 2026-03-26
+#    '/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_5.0.3_lae.txt', # updated 2025-09-26, matches Karls
 #    '/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_2D_Spectra_hdr5.0.0.txt', #8.0.22-k-fold updated 2025-05-17
 #    '/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_2D_Spectra_hdr5.0.0_dex.txt',#8.0.22_k-fold updated 2025-05-07
 #    '/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/lae_model_hdr5.0.0_lae_dex_small.txt',
     format='ascii'
 )
 #cnn_score_oii = Table.read('/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_2D_Spectra_hdr5.0.1_oii.txt', format='ascii')
-cnn_score_oii = Table.read('/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_5.0.3_oii.txt', format='ascii')        
+#cnn_score_oii = Table.read('/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_5.0.3_oii.txt', format='ascii')
+
+cnn_score_oii = Table.read('/scratch/projects/hetdex/hdr5/catalogs/ml/cnn_mukae/cnn_oii_20260316.txt', format='ascii')
+
 cnn_score = vstack([cnn_score_lae, cnn_score_oii])
 
 source_table2 = join( source_table, cnn_score, join_type='left')
@@ -150,6 +204,26 @@ for s in [20231014013,
 
 source_table['field'][ source_table['field'] == 'egs'] = 'dex-spring'
 
+for col in source_table.colnames:
+    if col == 'selected_det':
+        continue
+    if isinstance(source_table[col][0], str):
+        source_table[col] = source_table[col].astype(str)
+        print(col, ' is string')
+    try:
+        source_table[col] = source_table[col].filled(BADVAL)
+        #print(col)
+    except:
+        pass
+    
+    if col in ['sn','flux','flux_err','flux_aper','flux_aper_err','flux_lya','flux_lya_err','lum_lya','lum_lya_err','flux_oii','flux_oii_err','lum_oii','lum_oii_err']:
+        try:
+            sel_zero = source_table[col] == 0.0
+            source_table[col][sel_zero] = BADVAL
+            print('zero fill', col, np.sum(sel_zero))
+        except:
+            pass
+
 #add column to indicate if shot is used for cosmology
 #if version=='5.0.2':
 #    survey_use = Table.read( '/scratch/projects/hetdex/hdr5/survey/survey_use_{}.txt'.format('5.0.1'), format='ascii')
@@ -160,8 +234,10 @@ survey_use = Table.read( '/scratch/projects/hetdex/hdr5/survey/survey_use_{}.txt
 for s in [20170621008, 20180224008, 20180805010, 20180210012]:
     sel_shot = source_table['shotid'] == s
     print('Removing {} detections from {}'. format(np.sum(sel_shot), s))
+    print(len(source_table))
     source_table = source_table[np.invert( sel_shot)]
-    
+    print(len(source_table))
+          
 survey_use['flag_shot_cosmology'] = 1
 
 source_table2 = join( source_table, survey_use['shotid', 'flag_shot_cosmology'], join_type='left')
