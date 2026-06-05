77fa83574d5a4025bcf36c332a9e79f1695970c2
phantomamoeba <dustin.davis@utexas.edu>
2026-06-05 10:10:55 -0500

    test

--- hetdex_api/elixer_widget_cls.py
+++ hetdex_api/elixer_widget_cls.py
@@ -2722,14 +2722,17 @@ class ElixerWidget:
                     print(f"trying: {op.join(path, h5fn)}")
                     try:
                         handle = tables.open_file(op.join(path, h5fn))
+                        print(f"handle: {handle}")
                         if handle.__contains__("/elixer_reports"):
                             self.status_box.value += f"found: {op.join(path, h5fn)}"
                             print(f"found: {op.join(path, h5fn)}")
                             break #found a hit, always use user provided first then system
                                    #and exit the loop on the first hit that has reports
                         else:
+                            print(f"handle: does not have reports")
                             handle = None #found it, but it does not have reports in it
                     except:
+                        print(f"handle: exceptoin {traceback.format_exc()}")
                         continue
         except:
             self.status_box.value = f"Could not locate a suitable h5 file. {traceback.format_exc()}"
